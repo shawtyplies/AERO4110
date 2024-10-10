d9cfbffe6cedeea5657a82ebdafbdf97cefba5e9
Aisha <aishakozak@hotmail.com>
2024-10-10 22:39:45 +1100

    initial power calcs

---
+++ power.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+""" 
+Calculation of shaft power required from engine.
+
+Required mission performance parameters
+---------------------------------------
+Service ceiling height: 10,000 ft (minimum)
+Cruise height: 6,000 ft
+Runway requirements: 2,000 ft (minimum)
+Cruise speed: 100 kts (miniumum) (@ 6,000 ft)
+Climb rate: 900 fpm (minimum) (@ sea level)
+Range: 780 nmi (minimum) (maximum payload)
+Fuel reserves: 30 min
+
+Mission 1 parameters
+--------------------
+Range: 765 nmi
+Loiter: 10 min
+"""
+
+# Aircraft parameters
+MTOW = 1840 # lb
+W = MTOW*4.44822 # N
+rho = 1.225*0.06243 # lb/ft^3 (sea level)
+S = 150 # ft^2
+AR = 6
+delta = 0.058 # induced drag factor - check origin
+e = 1/(1+delta) # efficiency factor of wing
+k = 1/(np.pi*AR*e) # drag coefficient
+# print(f"k = {k}")
+C_D_0 = 0.025 # drag coefficient at zero lift (Torenbeek - light aircraft approximation)
+V_v = 900*60 # climb rate in ft/s (minimum)
+eta = 0.8 # propeller efficiency
+
+V_mp = np.sqrt(((2*W)/(rho*S))*np.sqrt(k/(3*C_D_0))) # ft/s
+print(f"V_mp = {V_mp} ft/s")
+print(f"V_mp = {V_mp*0.592484} knots")
