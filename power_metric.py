1f1aa1e5c0d6d91c940440645966f3444cc744b4
Aisha <aishakozak@hotmail.com>
2024-10-11 16:09:59 +1100

    power w assumed TAS max

--- power_metric.py
+++ power_metric.py
@@ -23,7 +23,8 @@ Loiter: 10 min
 MTOW = 834.6 # kg
 W = MTOW*9.81 # N
 # rho = 1.225/515.4 # slug/ft^3 (sea level)
-rho = 1.225 # lb/ft^3 (sea level)
+rho = 1.225 # kg/m^3 (sea level)
+rho_2 = 0.6601 # kg/m^3 (6,000 ft)
 S = 13.94 # m^2
 AR = 6
 delta = 0.058 # induced drag factor - check origin
@@ -32,11 +33,14 @@ k = 1/(np.pi*AR*e) # drag coefficient
 # print(f"k = {k}")
 C_D_0 = 0.025 # drag coefficient at zero lift (Torenbeek - light aircraft approximation)
 V_v = 4.572 # climb rate in m/s (minimum)
-eta = 0.85 # propeller efficiency
-V_mp = np.sqrt(((2*W)/(rho*S))*np.sqrt(k/(3*C_D_0))) # ft/sD
-# print(f"V_mp = {V_mp} ft/s")
-# print(f"V_mp = {V_mp*0.592484} knots")
+eta = 0.8 # propeller efficiency
+V_mp = np.sqrt(((2*W)/(rho*S))*np.sqrt(k/(3*C_D_0))) # m/s
+V_max = 130/1.944 # m/s (TAS)
 
-P_br = ((V_v+((rho*S*C_D_0)/(2*W)*V_mp**3)+(((2*k*W)/(rho*S))*V_mp**-1))*W)/eta # Watts
+P_br = ((V_v+((rho*S*C_D_0)/(2*W)*V_max**3)+(((2*k*W)/(rho*S))*V_max**-1))*W)/eta # Watts
 print(f"Engine shaft power required = {P_br} W")
 print(f"Engine shaft power required = {P_br/745.7} hp")
+
+# P_req = ((0.5*rho*S*C_D_0)*V_mp**3)+(((2*k*W**2)/(rho*S))*V_mp**-1)
+# print(f"Required power = {P_req} W")
+# print(f"Required power = {P_req/745.7} hp")
