c002bc47b43d8dee661a792241c44869f8f10059
Aisha <aishakozak@hotmail.com>
2024-10-16 11:30:54 +1100

    cooling area

--- propeller_sizing.py
+++ propeller_sizing.py
@@ -1,6 +1,7 @@
 import numpy as np
 from power_metric import bhp
 from power import V_max
+from power import V_v
 
 """
 Calculation of propeller size required for initial configuration.
@@ -27,11 +28,16 @@ D = K_p*pow(bhp,1/4) # ft
 print(f"Required prop diameter = {D} ft")
 print(f"Required prop diameter = {D*12} in")
 # print(f"Required prop diameter: {D_met} m")
+D_new = 70/12 # ft
 
 # Speed calcs 
 # For metal propeller, helical tip speed should not exceed 950 fps {290 m/s} at sea level.
 n = 2700/60 # rps from engine data
-V_tip_stat = np.pi*n*D
+V_tip_stat = np.pi*n*D_new
 print(f"V_tip_static = {V_tip_stat} ft/s")
 V_tip_hel = np.sqrt(V_tip_stat**2+(V_max)**2)
 print(f"V_tip_helical = {V_tip_hel} ft/s")
+
+# Cooling area
+A_cool = bhp/(2.2*V_v) # ft^2
+print(f"Required cooling area = {A_cool} ft^2")
